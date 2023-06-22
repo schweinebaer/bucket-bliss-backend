5bfdc477c11155cb8579657de74c599f60a38c96
benebreit <benedikt.breitschopf@gmail.com>
2023-06-22 05:56:51 +0200

    make it better 4

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:8101",
   "http://localhost:8100",
-  "https://bucket-bliss-8484522aca2f.herokuapp.com"
 ];
 
 // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
@@ -29,7 +28,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-mongoose.connect('mongodb+srv://benediktbreitschopf:YUCvRlzKsw44hdAB@cluster0.8l7sc86.mongodb.net/db', { useNewUrlParser: true }).then(() => {
+mongoose.connect(process.env.URI, { useNewUrlParser: true }).then(() => {
   const app = express();
 
   // Enable preflight requests for all routes
@@ -37,7 +36,7 @@ mongoose.connect('mongodb+srv://benediktbreitschopf:YUCvRlzKsw44hdAB@cluster0.8l
 
   app.use("/api", cors(corsOptions), routes); // new
 
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 5000, () => {
     console.log("Server has started!");
   });
 });
