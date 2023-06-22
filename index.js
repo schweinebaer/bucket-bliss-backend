a2d4a0750f08304d845fc75fe22f2c62e8159a7b
benebreit <benedikt.breitschopf@gmail.com>
2023-06-22 06:13:05 +0200

    make it better 6

--- index.js
+++ index.js
@@ -4,37 +4,15 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors = require("cors");
 
-const allowedOrigins = [
-  "capacitor://localhost",
-  "ionic://localhost",
-  "http://localhost",
-  "http://localhost:8080",
-  "http://localhost:8101",
-  "http://localhost:8100",
-];
-
-// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Origin not allowed by CORS"));
-    }
-  },
-};
-
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 mongoose.connect(process.env.URI, { useNewUrlParser: true }).then(() => {
   const app = express();
+  app.use(cors())
 
-  // Enable preflight requests for all routes
-  app.options("*", cors(corsOptions));
-
-  app.use("/api", cors(corsOptions), routes); // new
+  app.use("/api", routes); // new
 
   app.listen(process.env.PORT || 5000, () => {
     console.log("Server has started!");
